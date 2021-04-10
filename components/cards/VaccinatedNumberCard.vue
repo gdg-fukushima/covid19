59198995f20d5431218d48afa852ad62b3af6e61
shinbunbun <takamuneyuto@gmail.com>
2021-04-10 21:10:02 +0900

    fix eslint

--- components/cards/VaccinatedNumberCard.vue
+++ components/cards/VaccinatedNumberCard.vue
@@ -19,8 +19,8 @@
 
 <script>
 import TimeBarChartTwo from '../TimeBarChartTwo.vue'
-import formatGraph from '@/utils/formatGraph'
 import Data from '@/data/data.json'
+import formatGraph from '@/utils/formatGraph'
 
 export default {
   components: {
